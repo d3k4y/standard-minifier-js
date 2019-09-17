63db923d89a9f83b49e3cb8346bb2a14129bb590
Daniel Kloeters <dkloeters@hax-solutions.de>
2019-09-17 17:37:54 +0200

    changed package namespace to d3k4y; raised micro version to 2.1.1; raised dependency: babel-compiler@7.3.4; changed dependency to d3k4y:minifier-js@2.1.1

--- package.js
+++ package.js
@@ -1,25 +1,25 @@
 Package.describe({
-  name: 'abernix:standard-minifier-js',
-  version: '2.1.0',
-  summary: 'Harmony javascript minifiers used with Meteor apps by default.',
-  documentation: 'README.md',
+    name: 'd3k4y:standard-minifier-js',
+    version: '2.1.2',
+    summary: 'Harmony javascript minifiers used with Meteor apps by default.',
+    documentation: 'README.md',
 });
 
 Package.registerBuildPlugin({
-  name: "minifyStdJS",
-  use: [
-    'abernix:minifier-js@2.1.0',
-    'babel-compiler@6.19.2',
-    'ecmascript@0.8.0'
-  ],
-  sources: [
-    'plugin/minify-js.js',
-    'plugin/stats.js',
-    'plugin/visitor.js',
-    'plugin/utils.js',
-  ],
+    name: "minifyStdJS",
+    use: [
+        'd3k4y:minifier-js@2.1.0',
+        'babel-compiler@7.3.4',
+        'ecmascript@0.8.0'
+    ],
+    sources: [
+        'plugin/minify-js.js',
+        'plugin/stats.js',
+        'plugin/visitor.js',
+        'plugin/utils.js',
+    ],
 });
 
 Package.onUse(function(api) {
-  api.use('isobuild:minifier-plugin@1.0.0');
+    api.use('isobuild:minifier-plugin@1.0.0');
 });
